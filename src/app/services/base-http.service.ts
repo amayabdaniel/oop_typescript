bca66f8698981041f280655a2c6f9be8ab617f55
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2023-01-02 08:03:14 -0500

    add base-http service

---
+++ src/app/services/base-http.service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { Category } from "../models/category.model";
+import { Product } from "../models/products.model";
+
+export class BasteHttpService<TypeClass> {
+  //private data: TypeClass[] = [];
+
+  constructor(private url: string) {
+
+  }
+
+  async getAll() {
+    const { data } = await axios.get<TypeClass[]>(this.url);
+    return data;
+  }
+}
+
+
+
+(async () => {
+  const url1 = 'https://api.escuelajs.co/api/v1/products/';
+  const productService = new BasteHttpService<Product>(url1);
+
+  const rta1 = await productService.getAll();
+  console.log('products', rta1.length);
+
+  const url2 = 'https://api.escuelajs.co/api/v1/categories/';
+  const categoryService = new BasteHttpService<Category>(url1);
+
+  const rta2 = await categoryService.getAll();
+  console.log('categories', rta2.length);
+
+})();
